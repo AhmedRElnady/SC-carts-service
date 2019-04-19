b2affa74db013a50ad4f3d33193ba40431677075
ahmed elnady <dev.elnady@gmail.com>
2019-04-19 02:11:22 +0200

    authorize middleware

--- src/api/middlewares/authorize.middleware.js
+++ src/api/middlewares/authorize.middleware.js
@@ -1,7 +1,41 @@
+const config = require('config');
+
 function authorize() {
     return (req, res, next) => {
-        console.log(">>>> authorized >>>>>");
-        next();
+        (async () => {
+        
+            const tokenPayload = JSON.parse(req.headers['x-payload-header']),
+                userId = tokenPayload.id,
+                userRole = tokenPayload.role,
+                reqResource = req.route.path,
+                reqMethod = req.method.toLowerCase(),
+                roles = config.get('acl.roles');
+
+            for (let role in roles) {
+                if (role === userRole) {
+                    const roleObj = roles[role],
+                        allowedResources = roleObj.resources,
+                        allowedPermissions = roleObj.permissions;
+
+                    if (userRole === 'CUSTOMER') { // each customer can only access his own cart
+                        const cartId = tokenPayload.cartId,
+                            requestedCartId = req.params && req.params.cartId;
+
+                        if (!cartId || (requestedCartId !== cartId))
+                            return res.json({ msg: "Insufficient permissions to access resource" })
+                    }
+
+                    if (allowedResources.includes(reqResource) && allowedPermissions.includes(reqMethod)) {
+                        next();
+                    } else {
+                        res.json({ msg: "Insufficient permissions to access resource" }) // 403
+                    }
+
+                    break;
+                }
+            }
+            
+        })();
     }
 }
 
